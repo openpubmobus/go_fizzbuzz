fc8220ec85f8ed8126ccc3afb68be43a78cf0431
Jeff L <jeff@langrsoft.com>
2020-10-13 21:36:58 -0600

    ready for next time

--- src/pubmob/fizzbuzz.go
+++ src/pubmob/fizzbuzz.go
@@ -2,7 +2,23 @@ package main
 
 import "strconv"
 
+const Fizz = "Fizz"
+const Buzz = "Buzz"
+
 func FizzBuzz(number int) string {
+
+	divisibleBy3 := number%3 == 0
+	divisibleBy5 := number%5 == 0
+
+	if divisibleBy3 && divisibleBy5 {
+		return Fizz + Buzz
+	}
+	if divisibleBy3 {
+		return Fizz
+	}
+	if divisibleBy5 {
+		return Buzz
+	}
+
 	return strconv.Itoa(number)
 }
-
--- src/pubmob/fizzbuzz_test.go
+++ src/pubmob/fizzbuzz_test.go
@@ -1,21 +1,40 @@
 package main
 
-import "testing"
+import (
+	"testing"
+
+	. "github.com/stretchr/testify/assert"
+)
 
 func TestRegularNumbersReturnThemselves(t *testing.T) {
-	actual := FizzBuzz(1)
-	expected := "1"
-
-	if actual != expected {
-		t.Errorf("actual %q expected %q", actual, expected)
-	}
+	Equal(t, "1", FizzBuzz(1))
+	Equal(t, "2", FizzBuzz(2))
 }
 
-func TestAnotherTest(t *testing.T) {
-	actual := FizzBuzz(2)
-	expected := "2"
+func TestNumbersDivisibleBy3(t *testing.T) {
+	Equal(t, "Fizz", FizzBuzz(3))
+	Equal(t, "Fizz", FizzBuzz(27))
+}
 
-	if actual != expected {
-		t.Errorf("actual %q expected %q", actual, expected)
+func TestNumbersDivisibleBy5(t *testing.T) {
+	Equal(t, "Buzz", FizzBuzz(5))
+	Equal(t, "Buzz", FizzBuzz(10))
+}
+
+func TestNumbersDivisibleBy3and5(t *testing.T) {
+	Equal(t, "FizzBuzz", FizzBuzz(15))
+	Equal(t, "FizzBuzz", FizzBuzz(30))
+}
+
+func TestOneThroughFifteen(t *testing.T) {
+	Equal(t, "1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz",
+		TempName(15))
+}
+
+func TempName(limit int) string {
+	s := FizzBuzz(1)
+	for i := 2; i <= limit; i++ {
+		s += " " + FizzBuzz(i)
 	}
+	return s
 }
